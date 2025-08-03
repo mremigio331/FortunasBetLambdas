314736280191eb083dc943693d3701fcc60788a4
MattRemy <m.remigio331@gmail.com>
2025-08-02 18:48:12 -0700

    Updating Staging text to Testing.

--- run_local_api.sh
+++ run_local_api.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 export STAGE="Dev"
-export COGNITO_USER_POOL_ID=$(aws cognito-idp list-user-pools --region us-west-2 --max-results 60 --query "UserPools[?Name=='FortunasBet-UserPool-Staging'].Id" --output text)
+export COGNITO_USER_POOL_ID=$(aws cognito-idp list-user-pools --region us-west-2 --max-results 60 --query "UserPools[?Name=='FortunasBet-UserPool-Testing'].Id" --output text)
 export COGNITO_CLIENT_ID=$(aws cognito-idp list-user-pools --max-results 60 --region us-west-2 \
 --query "UserPools[].Id" --output text | xargs -n1 -I {} aws cognito-idp list-user-pool-clients \
     --user-pool-id {} --region us-west-2 \
-    --query "UserPoolClients[?contains(ClientName, 'FortunasBetUserPoolClientStaging')].ClientId" \
+    --query "UserPoolClients[?contains(ClientName, 'FortunasBetUserPoolClientTesting')].ClientId" \
     --output text
 )
 export COGNITO_REGION="us-west-2"
 export COGNITO_API_REDIRECT_URI="http://localhost:5000/"
-export COGNITO_DOMAIN="https://fortunasbet-staging.auth.us-west-2.amazoncognito.com"
-export API_URL="https://api.staging.fortunasbet.com"
-export TABLE_NAME="FortunasBet-UserTable-Staging"
+export COGNITO_DOMAIN="https://fortunasbet-testing.auth.us-west-2.amazoncognito.com"
+export API_URL="https://api.testing.fortunasbet.com"
+export TABLE_NAME="FortunasBet-UserTable-Testing"
 
 uvicorn app:app --reload --port 5000
